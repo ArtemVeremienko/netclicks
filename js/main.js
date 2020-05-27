e635afdfdcad21dc34a21a96e81a98a119c10fc1
Artem <temaspsxxx@gmail.com>
2020-05-27 20:08:27 +0300

    Получение данных с сервера и рендеринг карточек

--- js/main.js
+++ js/main.js
@@ -1,10 +1,63 @@
 'use strict';
 
-// меню
+const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+const API_KEY = '764f5f90718a44fc2bcf08b2dd691ef7';
+
 const leftMenu = document.querySelector('.left-menu'),
   hamburger = document.querySelector('.hamburger'),
   tvShowsList = document.querySelector('.tv-shows__list'),
-  modal = document.querySelector('.modal')
+  modal = document.querySelector('.modal');
+
+
+const DBService = class {
+  getData = async (url) => {
+    const res = await fetch(url);
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error(`Не удалось получить данные по адресу ${url}`)
+    }
+  }
+
+  getTestData = () => {
+    return this.getData('test.json');
+  }
+};
+
+const renderCard = response => {
+  tvShowsList.textContent = '';
+
+  response.results.forEach(item => {
+    const {
+      backdrop_path: backdrop,
+      name: title,
+      poster_path: poster,
+      vote_average: vote
+    } = item;
+
+    const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
+    const backdropIMG = backdrop ? IMG_URL + backdrop : '';
+    const voteElem = vote ? `<span class="tv-card__vote">${vote}</span>` : '';
+
+    const card = document.createElement('li');
+    card.className = 'tv-shows__item';
+    card.innerHTML = `
+      <a href="#" class="tv-card">
+          ${voteElem}
+          <img class="tv-card__img"
+              src="${posterIMG}"
+              data-backdrop="${backdropIMG}" alt="${title}">
+          <h4 class="tv-card__head">${title}</h4>
+      </a>
+    `;
+    tvShowsList.append(card);
+  });
+
+
+}
+
+new DBService().getTestData()
+  .then(renderCard);
 
 // открытие/закрытие меню
 hamburger.addEventListener('click', () => {
